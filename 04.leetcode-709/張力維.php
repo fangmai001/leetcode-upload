b9de731b8d89c9eef1a2dca6034bdefea2f39886
clw052793 <m272350666@gmail.com>
2023-11-21 15:34:52 +0800

    張力維

--- 04.leetcode-709/張力維.php
+++ 04.leetcode-709/張力維.php
@@ -1,15 +1,6 @@
 <?php
 class Solution {
-    public:
-        string toLowerCase(string s) {
-            $str1="Hello";
-            echo strtolower($str1)."<br>";
-
-            $str2="here";
-            echo strtolower($str2)."<br>";
-
-            $str3="LOVELY";
-            echo strtolower($str3)."<br>";
-            
-        }
-    };
+    def toLowerCase(self , s: str ) -> str:
+    r = s.lower()
+    return r
+}
