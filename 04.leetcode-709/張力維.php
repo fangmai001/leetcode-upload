7a9cdb06ca995ca536ad0de7e4113a16f2435314
clw052793 <m272350666@gmail.com>
2023-11-21 15:25:54 +0800

    張力維

---
+++ 04.leetcode-709/張力維.php
@@ -0,0 +1,15 @@
+<?php
+class Solution {
+    public:
+        string toLowerCase(string s) {
+            $str1="Hello";
+            echo strtolower($str1)."<br>";
+
+            $str2="here";
+            echo strtolower($str2)."<br>";
+
+            $str3="LOVELY";
+            echo strtolower($str3)."<br>";
+            
+        }
+    };
