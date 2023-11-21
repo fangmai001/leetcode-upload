fc7895be223588514ca7625480a1d302389edbaf
qike318 <qike318@gmail.com>
2023-11-21 15:22:16 +0800

    B18C008

---
+++ 04.leetcode-709/b18c008-709.php
@@ -0,0 +1,14 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $s
+     * @return String
+     */
+    function toLowerCase($s) {
+        $e=strtolower($s);
+        return $e;
+    }
+
+}
+?>
